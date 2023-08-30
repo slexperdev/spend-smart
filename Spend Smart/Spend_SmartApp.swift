805338ee8b3e083f562eff9fdce98f29a556a02a
slexperdev <sandunkasthuri1998@gmail.com>
2023-08-30 23:47:55 +0530

    Initial Commit

---
+++ Spend Smart/Spend_SmartApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spend_SmartApp.swift
+//  Spend Smart
+//
+//  Created by Sandun Kasthuri on 2023-08-30.
+//
+
+import SwiftUI
+
+@main
+struct Spend_SmartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
